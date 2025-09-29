5d12008c32a9fd14a2acba1125c336ef1e014f93
B00162515 Erica Cuthbert <B00162515@mytudublin.ie>
2025-09-29 16:19:02 +0100

    Completed Step 2

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import {View, Text, TextInput} from 'react-native';
 import React, {useState} from 'react';
+import {View, Text, TextInput} from 'react-native';
 import Logo from './components/Logo';
 
 
 export default function App(){
   const [fullname, setFullname] = useState("Erica");
+  const [fname, setFname] = useState("Joe");
+  const [lname, setLname] = useState("Bloggs");
+  const [dob, setDob] = useState("22 August 1990");
+  
   return(
     <View>
-      <logo/>
+      <Logo/>
       <Text>Hello, World {fullname}</Text>
       <TextInput placeholder="enter your name" onChangeText={setFullname}/>
+      <TextInput placeholder="Enter your firstname" onChangeText={setFname}/>
+      <TextInput placeholder="Enter your lastname" onChangeText={setLname}/>
+      <TextInput placeholder="Enter your date of birth" onChangeText={setDob}/>
+      <Text>Hello {fname} {lname}. You were born on {dob}</Text>
     </View>
   )
 };
